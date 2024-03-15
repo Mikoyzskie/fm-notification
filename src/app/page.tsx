bcd957e29cd395b3744d1553b87fca1dfe99a315
Mikoyzskie <rapidstrike13@gmail.com>
2024-03-15 23:19:29 +0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,75 @@
 import Image from "next/image";
+import clsx from "clsx";
+
+interface INotification {
+  unread: boolean;
+  image: string;
+  name: string;
+  time: string;
+  post?: string;
+  action: string;
+  isClub: boolean;
+}
+
+const notifications: INotification[] = [
+  { unread: true, image: "/avatar-mark-webber.webp", name: "Mark Webber", time: "1m", post: "My first tournament today!", action: "reacted to your recent post", isClub: false },
+  { unread: true, image: "/avatar-angela-gray.webp", name: "Angela Gray", time: "5m", action: "followed you", isClub: false },
+  { unread: true, image: "/avatar-jacob-thompson", name: "Jacob Thompson", time: "1 day", action: "avatar-jacob-thompson", isClub: true },
+]
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="min-h-screen h-full flex items-center justify-center">
+      <div className="max-w-[730px] w-full rounded-[15px] px-[30px] bg-white pt-[33px]">
+        <div className="flex items-center justify-between mb-[31px]">
+          <div className="flex items-center gap-[11px]">
+            <h2 className="text-[#1C202B] text-2xl font-extrabold">Notification</h2>
+            <span className="font-extrabold h-[25px] w-[35px] flex items-center justify-center text-white bg-[#0A327B] rounded-md">3</span>
+          </div>
+          <p className="text-[#5E6778]">Mark all as read</p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="flex flex-col gap-2">
+          <Unread unread={true} image="/avatar-mark-webber.webp" name="Mark Webber" time="1m" post="My first tournament today!" action="reacted to your recent post" isClub={false} />
+          <Unread unread={true} image="/avatar-angela-gray.webp" name="Angela Gray" time="5m" action="followed you" isClub={false} />
+          <Unread unread={true} image="/avatar-angela-gray.webp" name="Angela Gray" time="5m" action="followed you" isClub={true} post="Chess Club" />
+          {
+            notifications.map((notif: INotification, index: number) => {
+              return (
+                
+              )
+            })
+          }
+        </div>
       </div>
     </main>
   );
+}
+
+
+function Unread({ unread, image, name, time, post, action, isClub }: INotification) {
+  return (
+    <div className={clsx("py-[18px] px-5 rounded-lg flex items-center gap-5",
+      unread ? "bg-[#F7FAFD]" : ""
+    )}>
+      <Image
+        src={image}
+        alt="mark webber"
+        height={90}
+        width={90}
+        className="h-[45px] w-[45px]"
+      />
+      <div >
+        <div className="flex gap-[6px] items-center">
+          <p className="flex gap-[7px]">
+            <span className="text-[#1C202B] font-extrabold">{name}</span> <span className="text-[#5E6778]">{action}</span> <span className={clsx(" font-extrabold", post ? "" : "hidden", isClub ? "text-[#0A327B]" : "text-[#5E6778]")}>{post}</span>
+          </p>
+          <svg className={clsx("", unread ? "" : "hidden")} width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="4" cy="4" r="4" fill="#F65552" />
+          </svg>
+
+        </div>
+        <p className="text-[#5E6778]">{time} ago</p>
+      </div>
+    </div>
+  )
 }
